4b702bcf8e664bab9618aa6b05057f872d221a8d
arti260495 <arti260495@gmail.com>
2021-11-25 16:37:03 +0300

    Уровень 2 - ДЗ5

---
+++ src/Level2/Lesson5/MyThread.java
@@ -0,0 +1,25 @@
+package Level2.Lesson5;
+
+public class MyThread extends Thread {
+    private float[] arr;
+
+    MyThread(String name, float[] arr) {
+        super(name);
+        this.arr = arr;
+    }
+
+     float[] getArr() {
+        return arr;
+    }
+
+    @Override
+    public void run() {
+        calculate();
+    }
+
+    private void calculate() {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+    }
+}
