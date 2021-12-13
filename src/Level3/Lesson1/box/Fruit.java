422d2b4eaa013d7d4e255c972617fa5815feed8e
arti260495 <arti260495@gmail.com>
2021-12-13 19:43:59 +0300

    Уровень 3 - ДЗ1

---
+++ src/Level3/Lesson1/box/Fruit.java
@@ -0,0 +1,14 @@
+package Level3.Lesson1.box;
+
+public abstract class Fruit {
+
+    private float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    float getWeight() {
+        return weight;
+    }
+}
---
+++ src/Level3/Lesson1/box/Orange.java
@@ -0,0 +1,8 @@
+package Level3.Lesson1.box;
+
+public class Orange extends Fruit {
+
+    public Orange() {
+        super(1.5f);
+    }
+}
